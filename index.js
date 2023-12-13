28c546b872bb30581af569fa9f0173ae9fc69e8a
emilymorosoff <emilymorosoff@icloud.com>
2023-12-12 20:01:45 -0800

    Add CLI prompts for user input in index.js.

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const inquirer = require('inquirer');
 const { Triangle, Circle, Square, Rectangle, Ellipse, Heart } = require('./lib/shapes');
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'text',
+        message: 'What text do you want on the logo? (max 3 characters)',
+        validate: input => input.length <= 3 || 'Text must be a maximum of 3 characters long'
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What color do you want the text to be? (name or hex code)',
+        default: 'black'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape for your logo:',
+        choices: ['Triangle', 'Circle', 'Square', 'Rectangle', 'Ellipse', 'Heart']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'What color do you want the shape to be? (name or hex code)',
+        default: 'blue'
+    }
+]).then(answers => {
+    createLogo(answers);
+}).catch(error => {
+    console.error('An error occurred:', error);
+});
